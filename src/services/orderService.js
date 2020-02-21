04058a86968d8ced465490ccd20b15ad7fdd9a61
Jeffrey W <jeffrey.wu@smoothstack.com>
2020-02-20 23:54:14 -0500

    Removed console logs

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -26,14 +26,5 @@ exports.authorize = request =>
         return reject(err);
       }
       return resolve(decoded);
-      // User.findById(decoded.id,
-      // { password: 0 }, // projection
-      // function (err, user) {
-      //   if (err) return res.status(500).send("There was a problem finding the user.");
-      //   if (!user) return res.status(404).send("No user found.");
-
-      //   // res.status(200).send(user); Comment this out!
-      //   next(user); // add this line
-      // });
     }); 
   });
